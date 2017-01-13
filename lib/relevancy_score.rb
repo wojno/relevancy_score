7f9090a6a558b92cc33613c9722210fd35fc52bb
Ryan Wojno <wojno2112@gmail.com>
2017-01-12 21:21:52 -0500

    - updates to relevancy_score to load all required libs and yaml

--- lib/relevancy_score.rb
+++ lib/relevancy_score.rb
@@ -1,5 +1,7 @@
-require "relevancy_score/version"
+require 'yaml'
+Dir.glob('./lib/relevancy_score/*', &method(:require))
 
+# Calculate the relevancy of a search term against a text blob
 module RelevancyScore
   # Your code goes here...
 end
