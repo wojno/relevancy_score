5a646bf6569403e75bcfc40442f2001ee70a72c3
Ryan Wojno <wojno2112@gmail.com>
2017-01-12 22:52:01 -0500

    - added MeaningfulWords class to return a count of significant words - class relies upon a stopwords file that can be swapped in / out - adjusted main Calculate.score method to round to 5 decimal places   to aid readability and updated tests accordingly

--- test/relevancy_score_test.rb
+++ test/relevancy_score_test.rb
@@ -6,6 +6,6 @@ class RelevancyScoreTest < Minitest::Test
   end
 
   def test_that_it_returns_a_score
-    assert_equal(0.045, RelevancyScore::Calculate.new(text: @text, keyword: 'Jedi').score)
+    assert_equal(0.01654, RelevancyScore::Calculate.new(text: @text, keyword: 'Jedi').score)
   end
 end
